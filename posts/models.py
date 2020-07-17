3b477e9a0631219f5447693c9c43e74c8e403f19
Kdheeru12 <60852451+Kdheeru12@users.noreply.github.com>
2020-07-17 23:02:46 +0530

    detail

--- posts/models.py
+++ posts/models.py
@@ -6,11 +6,21 @@ from django.utils.text import slugify
 # Create your models here.
 class Post(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,default=1,on_delete=models.CASCADE)    
-    title = models.CharField(max_length=120)
+    title = models.CharField(max_length=400)
     slug = models.SlugField(unique=True)  
     email = models.EmailField()
     image = models.ImageField(blank=True,null=True)
+    heading1 = models.CharField(max_length=400,null=True)
+    para1    = models.TextField(null=True)
     content = models.TextField()
+    heading2 = models.CharField(max_length=400,null=True)
+    content1 = models.TextField(null=True)
+    heading3 = models.CharField(max_length=400,null=True)
+    content2 = models.TextField(null=True)
+    heading4 = models.CharField(max_length=400,null=True)
+    content3 = models.TextField(null=True)
+    
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="likes", blank=True)
     template_name = models.CharField(max_length=100)
     updated = models.DateTimeField(auto_now=True,auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False,auto_now_add=True)
@@ -20,6 +30,8 @@ class Post(models.Model):
         return self.title
     def get_absolute_url(self):
         return "/%s/post-detail/" %(self.slug)
+    def total_likes(self):
+        return self.likes.count()
     class Meta:
         ordering = ["-timestamp","-updated"]  
 def create_slug(instance,new_slug=None):
--- posts/views.py
+++ posts/views.py
@@ -109,29 +109,88 @@ def createpost(request):
         content = request.POST["content"]
         email = request.POST["email"]
         user = request.user
-        print(title , template ,content) 
-        instance = Post(title=title,template_name=template,content=content,email=email,user=user)
-        instance.save()       
+        if template == 'style1':
+            print(title , template ,content) 
+            instance = Post(title=title,template_name=template,content=content,email=email,user=user)
+            instance.save()       
+        elif template =='style2':
+            heading1 = request.POST["heading1"]
+            para = request.POST["para"]
+            heading2 = request.POST["heading2"]
+            content1 = request.POST["content1"]
+            heading3 = request.POST["heading3"]
+            content2 = request.POST["content2"]
+            instance = Post(title=title,template_name=template,content=content,email=email,user=user,heading1=heading1,para1=para,heading2=heading2,heading3=heading3,content1=content1,content2=content2)
+            instance.save()
+                  
+        elif template =='style3':
+            heading1 = request.POST["heading1"]
+            heading2 = request.POST["heading2"]
+            content1 = request.POST["content1"]
+            heading3 = request.POST["heading3"]
+            content2 = request.POST["content2"]
+            heading4 = request.POST["heading4"]
+            content3 = request.POST["content3"]
+            instance = Post(title=title,template_name=template,content=content,email=email,user=user,heading1=heading1,heading2=heading2,heading3=heading3,content1=content1,content2=content2,heading4=heading4,content3=content3)
+            instance.save()
+        elif template =='style4':
+            heading1 = request.POST["heading1"]
+            para = request.POST["para"]
+            instance = Post(title=title,template_name=template,content=content,email=email,user=user,heading1=heading1,para1=para)
+            instance.save()       
         return redirect('/myposts')
-       
     else:
         return render(request,'post.html')
 def style1(request):
-    return render(request,'template1.html')
+    return redirect('/error')
+def style2(request):
+    return render(request,'style2.html')
+def style3(request):
+    return render(request,'style3.html')
+def style4(request):
+    return render(request,'style4.html')
+def template1(request):
+    return render(request,'template3.html')
+def template2(request):
+    return render(request,'template2.html')
+def template3(request):
+    return render(request,'template3.html')
+def template4(request):
+    return render(request,'template4.html')
+
 def postdetail(request,slug=None):
     if slug == None:
     
         return redirect('signup')
     else:
         instance = get_object_or_404(Post,slug=slug)
+        is_liked =False
+        
+        if instance.likes.filter(username=request.user).exists():
+            is_liked = True
         context = {
             "title": instance.title,
             "instance": instance,
+            "is_liked" :is_liked,
+            "total_likes":instance.total_likes(),
         }
         return render(request,"detail.html",context)
+def likes(request):
+    slug=request.POST.get('like')
+    post = get_object_or_404(Post,slug=request.POST.get('like'))
+    is_liked =False
+    print(post.likes.filter(username=request.user).exists())
+    if post.likes.filter(username=request.user).exists():
+        
+        post.likes.remove(request.user)
+       
+        is_liked = False
+    else:
+        post.likes.add(request.user)
+        is_liked = True
+    return redirect('/' + str(slug)+'/post-detail' )
 def myposts(request):
     if request.user.is_authenticated:
-        print(request.user)
         post=Post.objects.filter(user=request.user)
         return render(request,"myposts.html",{'post':post})
     else:
@@ -148,14 +207,21 @@ def othersposts(request,user=None):
     return render(request,"others.html",context)
 def editpost(request,slug=None):
     if request.method == "POST":
+        like = []
         instance = get_object_or_404(Post,slug=slug)
-        print(instance)
+        globals()['likes']= instance.likes.all()
+        print('likes:',likes)
+        print('like:',like)
+        for users in likes:
+            print(users)
+            like.append(users)
+        globals()['users']=list(like)
+
         context = {
             "title": instance.title,
             "instance": instance,
         }
-        print(instance.content)
-        
+
         return render(request,'edit.html',context)
     else:
         return redirect('/error')
@@ -163,14 +229,26 @@ def editpost(request,slug=None):
 def save(request,slug=None):
     if request.method == "POST":
         instance = get_object_or_404(Post,slug=slug)
-        instance.delete()
         title = request.POST["title"]
         template = request.POST["template"]
         content = request.POST["content"]
         email = request.POST["email"]
         user = request.user
-        instance = Post(title=title,template_name=template,content=content,email=email,user=user,slug=slug)
+        para = request.POST["para"]
+        heading1 = request.POST["heading1"]
+        heading2 = request.POST["heading2"]
+        content1 = request.POST["content1"]
+        heading3 = request.POST["heading3"]
+        content2 = request.POST["content2"]
+        heading4 = request.POST["heading4"]
+        content3 = request.POST["content3"]
+        instance.delete()
+        print('likes:',users)
+        instance = Post(title=title,template_name=template,content=content,email=email,user=user,heading1=heading1,heading2=heading2,heading3=heading3,content1=content1,content2=content2,heading4=heading4,content3=content3,para1=para)
         instance.save()
+        post = get_object_or_404(Post,slug=slug)
+        for likes in users:
+            post.likes.add(likes)
         return redirect('/'+str(slug)+'/post-detail' )
     else:
         return redirect('/error')
