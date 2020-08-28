489fd0018efb2e5ae23a0dc236e62195ef73804f
Kdheeru12 <60852451+Kdheeru12@users.noreply.github.com>
2020-08-28 10:41:18 +0530

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -17,13 +17,18 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
+from django.views.static import serve
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 urlpatterns = [
     path('',include('posts.urls')),
     path('admin/', admin.site.urls),
-path("ckeditor5/", include('django_ckeditor_5.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path("ckeditor5/", include('django_ckeditor_5.urls')),
+]
 
-urlpatterns += staticfiles_urlpatterns()
+if not settings.DEBUG:
+    urlpatterns += [
+        path('media/')', serve,{'document_root': settings.MEDIA_ROOT}),
+        path('static/)', serve,{'document_root': settings.STATIC_ROOT}),
+    ]
